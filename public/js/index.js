169e4f378c9b028d6543a9daf170ea2789739e8e
Marco Luchi <marcoaluchi@hotmail.com>
2020-06-12 20:31:29 -0300

    se desarrollo el algoritmo para obtener las opciones seleccioandas

--- public/js/index.js
+++ public/js/index.js
@@ -5,8 +5,14 @@ ponerOpciones(opciones2);
 let btn = document.getElementById("btn");
 
 btn.addEventListener("click", (e) => {
+  let v1 = obtenerOpciones(opciones);
+  let v2 = obtenerOpciones(opciones2);
   fetch(
-    "https://free.currconv.com/api/v7/convert?q=USD_ARS&compact=ultra&apiKey=bc000818fffe49c95c15"
+    "https://free.currconv.com/api/v7/convert?q=" +
+      v1 +
+      "_" +
+      v2 +
+      "&compact=ultra&apiKey=bc000818fffe49c95c15"
   )
     .then((res) => res.json())
     .then((data) => {
@@ -19,15 +25,22 @@ function ponerOpciones(arg) {
   )
     .then((res) => res.json())
     .then((data) => {
-      let valores = data.results;
-      let currenciesName = Object.values(valores);
-      let sigla = Object.keys(valores);
+      const valores = data.results;
+      const currenciesName = Object.values(valores);
+      const sigla = Object.keys(valores);
       let cont = 0;
       sigla.forEach((elem) => {
         let option = document.createElement("option");
         option.textContent = elem + " - " + currenciesName[cont].currencyName;
+        option.id = cont;
         arg.appendChild(option);
         cont++;
       });
     });
+}
+function obtenerOpciones(o) {
+  const opcionElegida = o.selectedOptions;
+  let textoElegido = opcionElegida[0].textContent;
+  textoElegido = textoElegido.substr(0, 3);
+  return textoElegido;
 }
