5c13834a0afba062e041c7e2b4ce313112f1c645
Marco Luchi <marcoaluchi@hotmail.com>
2020-06-12 20:49:58 -0300

    se agrego funcionalidad apra sacar cuenta del resultado, con importe y todo

--- public/js/index.js
+++ public/js/index.js
@@ -1,12 +1,17 @@
 let opciones = document.getElementById("opciones");
 let opciones2 = document.getElementById("opciones2");
+let importe = document.getElementById("importe");
+let seccion = document.getElementById("resultado");
 ponerOpciones(opciones);
 ponerOpciones(opciones2);
 let btn = document.getElementById("btn");
 
 btn.addEventListener("click", (e) => {
+  let importeFinal = importe.value;
   let v1 = obtenerOpciones(opciones);
   let v2 = obtenerOpciones(opciones2);
+
+  borrarHijos();
   fetch(
     "https://free.currconv.com/api/v7/convert?q=" +
       v1 +
@@ -16,7 +21,11 @@ btn.addEventListener("click", (e) => {
   )
     .then((res) => res.json())
     .then((data) => {
-      console.log(data);
+      let concatenado = v1 + "_" + v2;
+      importeFinal *= data[concatenado];
+      let valorFinal = document.createElement("h3");
+      valorFinal.textContent = importeFinal;
+      seccion.appendChild(valorFinal);
     });
 });
 function ponerOpciones(arg) {
@@ -43,4 +52,14 @@ function obtenerOpciones(o) {
   let textoElegido = opcionElegida[0].textContent;
   textoElegido = textoElegido.substr(0, 3);
   return textoElegido;
+}
+function borrarHijos() {
+  var e = document.querySelector("#resultado");
+
+  //e.firstElementChild can be used.
+  var hijo = e.lastElementChild;
+  while (hijo) {
+    e.removeChild(hijo);
+    hijo = e.lastElementChild;
+  }
 }
